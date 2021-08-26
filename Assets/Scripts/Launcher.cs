889c8aa909bf3bfee85dbf7bffcbd543cec182e0
Jenni <justlittleoldmejenn@gmail.com>
2021-08-26 14:58:08 -0700

    Bird launches into space

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,6 +9,8 @@ public class Launcher : MonoBehaviour {
 
     [SerializeField] private Rigidbody2D rb;
     private Vector2 releasePosition;
+    private bool trackPosition = false;
+    [SerializeField] private float releaseTime = 0.15f;
 
     private void Start() {
         var input = new PlayerControls();
@@ -25,19 +28,39 @@ public class Launcher : MonoBehaviour {
         inputClick.performed += OnInputHold;
     }
 
-    private void OnInputRelease(InputAction.CallbackContext ctx) {
-        releasePosition = inputMovement.ReadValue<Vector2>();
-        Debug.Log($"Mouse click released at position: {releasePosition}");
-        rb.position = releasePosition;
-        rb.isKinematic = false;
+    // This needs refactoring but testing for now
+    private void Update() {
+        if (trackPosition) {
+            KeepBallOnMouse();
+        }
+    }
+
+    private void KeepBallOnMouse() {
+        rb.position = Camera.main.ScreenToWorldPoint(inputMovement.ReadValue<Vector2>());
     }
 
     private void OnInputClick(InputAction.CallbackContext ctx) {
+        trackPosition = true;
         Debug.Log($"Mouse clicked at position: {inputMovement.ReadValue<Vector2>()}");
         rb.isKinematic = true;
     }
 
+    private void OnInputRelease(InputAction.CallbackContext ctx) {
+        trackPosition = false;
+        releasePosition = inputMovement.ReadValue<Vector2>();
+        Debug.Log($"Mouse click released at position: {releasePosition}");
+        rb.position = releasePosition;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+    }
+
     private void OnInputHold(InputAction.CallbackContext ctx) {
-        rb.position = Camera.main.ScreenToWorldPoint(inputMovement.ReadValue<Vector2>());
+        // rb.position = Camera.main.ScreenToWorldPoint(inputMovement.ReadValue<Vector2>());
+        //  Debug.Log($"Mouse being held down: {inputMovement.ReadValue<Vector2>()}");
+    }
+
+    private IEnumerator Release() {
+        yield return new WaitForSeconds(releaseTime);
+        GetComponent<SpringJoint2D>().enabled = false;
     }
 }
