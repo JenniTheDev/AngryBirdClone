b806d9ca1b5b5128c70eb5110ed8e9ad85028597
Jenni <justlittleoldmejenn@gmail.com>
2021-09-10 09:41:11 -0700

    Saving, Ball launch isn't working right

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,4 +2,9 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Ball : MonoBehaviour {
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        Debug.Log("Ball is out of bounds");
+        // Make this a SO Event to reset ball
+    }
 }
